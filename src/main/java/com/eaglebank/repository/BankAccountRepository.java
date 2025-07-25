58665a423373b3643ce86e82855aab49355cad21
Abhinav Mehrotra <abhinavm_123@yahoo.com>
2025-07-25 11:40:56 +0100

    refactored code and tests

--- src/main/java/com/eaglebank/repository/BankAccountRepository.java
+++ src/main/java/com/eaglebank/repository/BankAccountRepository.java
@@ -3,5 +3,10 @@ package com.eaglebank.repository;
 import com.eaglebank.model.BankAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+    boolean existsByUserId(Long userId);
+
+    List<BankAccount> findAllByUserId(Long userId);
 }
